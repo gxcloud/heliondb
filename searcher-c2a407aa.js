832d279821e11132f4ecc6c88a338db094aebf74
alexgauss <alexgauss@users.noreply.github.com>
2026-05-13 15:57:17 +0000

    deploy: 0b6686e7dfcba93055f7c68b0dad102c3be4f72c

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d0ce92a3.js',
+                path_to_root + 'searchindex-7a5e4ae9.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
